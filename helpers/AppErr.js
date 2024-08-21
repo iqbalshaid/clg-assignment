e44d479899546c45d9201e4782c0f9783a7dbcdd
iqbalshaid <111365870+iqbalshaid@users.noreply.github.com>
2024-08-21 15:33:16 +0530

    shah

---
+++ helpers/AppErr.js
@@ -0,0 +1,9 @@
+class AppErr extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
+      this.status = false;
+    }
+  }
+  
+  module.exports = AppErr;
